e7e65042b4d3361929b2e694d52ff3b21e006561
John Meehan <john.meehan1@gmail.com>
2015-02-08 17:29:42 +0000

    implemented signin

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
   resources :projects do
     resources :tickets
   end
+
+  get '/signin', to: "sessions#new"
+  post '/signin', to: "sessions#create"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
---
+++ spec/features/sign_in_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.feature "SignIn", type: :feature do
+  scenario "signing in via form" do
+    user = FactoryGirl.create(:user)
+    visit root_path
+    click_link 'Sign in'
+    fill_in 'Name', with: user.name
+    fill_in 'Password', with: user.password
+    click_button 'Sign in'
+
+    expect(page).to have_content("Signed in successfully.")    
+  end
+end
