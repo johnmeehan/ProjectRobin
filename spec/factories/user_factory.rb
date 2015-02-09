671c440238de4a39c1e11fbbdbc1ed896ea490c5
John Meehan <john.meehan1@gmail.com>
2015-02-09 11:06:40 +0000

    added admin flag to user

--- spec/factories/user_factory.rb
+++ spec/factories/user_factory.rb
@@ -1,7 +1,11 @@
 FactoryGirl.define do
   factory :user do
-    name "John"
-    password "password"
-    email "john@example.com"
+      name "John"
+      password "password"
+      email "john@example.com"
+
+      factory :admin_user do
+        admin true
+      end
     end
 end
