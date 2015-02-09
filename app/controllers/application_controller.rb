4f60365bdae06fda3860aff603c32ef5d8ad57bc
John Meehan <john.meehan1@gmail.com>
2015-02-09 19:27:14 +0000

    Add admin namespaced users controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,15 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  private
+  def authorize_admin!
+    require_signin!
+    unless current_user.admin?
+      flash[:alert] = "You must be an admin to do that."
+      redirect_to root_path
+    end
+  end
+
   def require_signin!
     if current_user.nil?
       flash[:error] = "You need to sign in or sign up before continuing."
