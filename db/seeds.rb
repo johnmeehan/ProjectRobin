eaffea72accb8904287b5ed507f127290095e914
John Meehan <john.meehan1@gmail.com>
2015-02-11 23:18:51 +0000

    Add the ability to attach a file to a ticket

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,4 +11,4 @@ admin_user = User.create(email: "admin@example.com",
 	password_confirmation: "password",
 	admin: true)
 
-Project.create(name: "Tictetee Beta")
+Project.create(name: "Ticketee Beta")
