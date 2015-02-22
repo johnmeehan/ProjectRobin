0e4d0e81e5ac04b763ceac9f608258397a1f1bf8
John Meehan <john.meehan1@gmail.com>
2015-02-22 14:47:41 +0000

    changed seed data for State color to better looking ones

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,6 +13,6 @@ User.create(email: "admin@example.com",
 	admin: true)
 Project.find_or_create_by(name: "Ticketee Beta")
 
-State.find_or_create_by(name: 'New', background: "#85FF00", color: 'white', default: true)
-State.find_or_create_by(name: 'Open', background: "#00CFFD", color: 'white')
-State.find_or_create_by(name: 'Closed', background: "black", color: 'white')
+State.find_or_create_by(name: 'New', background: "#f0ad4e", color: 'white', default: true)
+State.find_or_create_by(name: 'Open', background: "#5cb85c", color: 'white')
+State.find_or_create_by(name: 'Closed', background: "#333", color: 'white')
