396ebe665685f92b162bd2563e8980d9e0b88c6a
John Meehan <john.meehan1@gmail.com>
2015-02-26 22:37:23 +0000

    [finished #88962104] Down case email before saving

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,10 +12,15 @@
 #
 
 class User < ActiveRecord::Base
-  has_many :permissions
+	before_save { self.email = email.downcase }
+
+  has_many :permissions  
+
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 	validates :email, presence: true, uniqueness: { case_sensitive: false }, format: { with: VALID_EMAIL_REGEX }
+
   has_secure_password
+  
   def to_s
     "#{email} (#{admin? ? "Admin" : "User"})"
   end
